6c25c70fbd7097ef29dae4c52e0a6869e1ef0aaa
Калдар К. Н <KaldarKN@elcom.local>
2022-08-31 22:53:56 +0700

    Нужно будет дописать запросы и оптимизировать весь код

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import UserCard from './components/UserCard';
+import Reviews from './components/Reviews';
+import styles from './App.module.scss';
+import userPhoto from './assets/img/photo.jpg';
+import Footer from './components/Footer';
+import Form from './components/Form';
+import { useState } from 'react';
+
+const myInfo = {
+  name: 'Калдар Кайрат',
+  birthday: '27.06.1996',
+  city: 'Томск',
+  sex: 'мужчина',
+  age: '25',
+  info: `Привет! Меня зовут Кайрат, мне 25 лет. Уже 3.5 года работаю
+  инженером-программистом в сфере Автоматизации технологических процессов. Но сейчас всерьёз
+  задумываюсь о смене сферы деятельности: хочу стать Front-end разработчиком. Изучаю HTML,
+  CSS, native JS, ReactJS. Скоро залечу в сферу IT и стану классным разработчиком! 💻`,
+  ps: 'Кстати, у меня есть брат-близнец :)',
+  pet: 'нет',
+};
 
 function App() {
+  let [formOpened, setFormOpened] = useState(false);
+
+  const handlerOpenForm = () => {
+    setFormOpened(!formOpened);
+    window.scrollTo(0, 0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      {formOpened && <Form handlerOpenForm={handlerOpenForm} />}
+      <Header />
+      <div className={styles.content}>
+        <h1 className={styles.welcome}>Добро пожаловать в академию!</h1>
+        <img className={styles.avatar} src={userPhoto} alt="user-avatar" />
+        <div className={styles.card}>
+          <UserCard {...myInfo} />
+        </div>
+        <div className={styles.reviews}>
+          <Reviews handlerOpenForm={handlerOpenForm} />
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/ReviewCard/index.jsx
@@ -0,0 +1,18 @@
+import styles from './ReviewCard.module.scss';
+
+const ReviewCard = ({ photo, name, date, comment }) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.header}>
+        <div className={styles.author}>
+          <img src={photo} alt="userPhoto" />
+          <h1>{name}</h1>
+        </div>
+        <h3>{date}</h3>
+      </div>
+      <p>{comment}</p>
+    </div>
+  );
+};
+
+export default ReviewCard;
